7bfe384d254d5dae2269ce4038740525dd729d17
Charles <teamupwithcoachcarr@gmail.com>
2021-01-20 03:27:04 +0100

    Initial Commit

---
+++ macappsbblinker/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macappsbblinker
+//
+//  Created by Charles Carr on 1/20/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
